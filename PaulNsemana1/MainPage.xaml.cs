d73b8b60c4ffebffd8c2b56d0cd7db8613066d3d
PC <PC@DESKTOP-GF3EBS0>
2023-01-11 18:27:28 -0500

    Agregar archivos de proyecto.

---
+++ PaulNsemana1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PaulNsemana1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Btnbienvenida_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtnombre.Text;
+            string apellido = txtapellido.Text;
+            string edad = txtedad.Text;
+            DisplayAlert("Mensaje de Bienvenida", "Bienvenido: Tu nombre es " + nombre + " tu apellido es " + apellido + " y tienes " + edad + " años", "Cerrar");
+        }
+    }
+}
